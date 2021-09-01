fb627990e856e6e168e20c0fce788f86558c2f75
Ritesh Kumar shukla <riteshshukla2381@gmail.com>
2021-09-01 15:07:00 +0530

    First Commit

---
+++ public/js/form.js
@@ -0,0 +1,12 @@
+
+function  SEND(){
+var name=document.getElementById('input_name').value
+var email=document.getElementById('input_email').value
+var subject=document.getElementById('input_subject').value
+var message=document.getElementById('input_message').value
+console.log(name)
+console.log(email)
+console.log(subject)
+console.log(message)
+console.log("Hello")
+}
